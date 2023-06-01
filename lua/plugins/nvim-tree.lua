164a303a4dd74ac155f81a11772e733ea7a571cb
qzmei <qzmei1997@163.com>
2023-06-01 14:57:43 +0800

    debug nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,22 +1,17 @@
-return {
-	"nvim-tree/nvim-web-devicons",
-	"nvim-tree/nvim-tree.lua", -- file explorer
-	config = function()
-		require("nvim-tree").setup({
-			sort_by = "case_sensitive",
-			view = {
-				width = 30,
-			},
-			renderer = {
-				group_empty = true,
-			},
-			filters = {
-				dotfiles = true,
-			},
-			-- 不显示 git 状态图标
-			git = {
-				enable = false,
-			},
-		})
-	end,
-}
+require("nvim-tree").setup({
+	sort_by = "case_sensitive",
+	view = {
+		width = 30,
+	},
+	renderer = {
+		group_empty = true,
+	},
+	filters = {
+		dotfiles = true,
+	},
+	-- 不显示 git 状态图标
+	git = {
+		enable = false,
+	},
+})
+return {}
