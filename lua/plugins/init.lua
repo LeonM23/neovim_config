e643df4e58de2baff553b8d90105a22a50f39199
qzmei <qzmei1997@163.com>
2023-06-01 15:00:30 +0800

    debug

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,4 +3,6 @@ return {
 	"nvim-lua/plenary.nvim", -- dependencies of lots of plugins
 	-- UI
 	"p00f/nvim-ts-rainbow", -- rainbow pairs
+	"nvim-tree/nvim-web-devicons",
+	"nvim-tree/nvim-tree.lua", -- file explorer
 }
